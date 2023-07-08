786b0b05469475ed68ef1353a32dfb47d4b8761f
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2023-07-08 16:19:24 -0400

    done

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+const colors = {
+    green: {
+        1: "#83d0cb",
+        2: "#145277",
+    },
+    blue: {
+        1: "#9bafd9",
+        2: "#3d47d9",
+    },
+    purple: {
+        1: "#aa96f9",
+        2: "#552586",
+    },
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.purple,
+                secondary: colors.green,
+            },
+        },
+    },
+    plugins: [require("prettier-plugin-tailwindcss")],
+};

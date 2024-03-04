b64ae72e7d0a6fbec519fd1898350c92112ec3e1
Elijah Lieu <elijahlieu@protonmail.com>
2024-03-04 12:22:21 -0500

    Add vim handout

---
+++ public/handout.cpp
@@ -0,0 +1,135 @@
+// My name is John Doe.
+
+#include <iostream>
+
+using namespace sdt;
+
+void printSquared();
+
+int main() {
+    cout << "Hello, world!" << endl;
+    // insert a comment between this one and the one above!
+
+    // This is a DELETEME comment.
+    // snake_DELETEME_case_variable_name
+    // Delete this whole line.
+
+    // Delete after this colon: DELETEME
+    int change_me = 0;
+
+    int ab = 0;
+
+    // Here is HIGHLIGHT THIS another comment!
+    // Highlight these lines to copy:
+    // Block 1
+    // Block 2
+    // Block 3
+
+    // Paste them below this line!
+
+
+    // Replace all occurrences of bad_name to count
+    int bad_name = 0;
+    for (int i = 0; i < 10; ++i) {
+        if (bad_name & 1) {
+            ++bad_name;
+            cout << "bad_name = " << bad_name << endl;
+        }
+    }
+
+    // Delete this whole "paragraph"
+    if (false) {
+        cout << "This will never happen" << endl;
+        while (true);
+    }
+
+    // Paste the insides of this if statement
+    // to the function printSquared
+    if (true) {
+        int x;
+        cin >> "Give me a number: " << endl;
+        cout << "Your number squared is " << x * x << endl;
+    }
+
+    // Use a macro to take each of these variables
+    // and print it in a form like macro 1!
+    int macro1 = 10;
+    cout << "macro1 = " << macro1 << endl;
+    int macro2 = 20;
+    int macro3 = 30;
+    int macro4 = 40;
+    int macro5 = 50;
+    int macro6 = 60;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void printSquared() {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is a secret message at the bottom of the page!
